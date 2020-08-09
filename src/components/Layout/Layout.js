9db53fd795f59d44fb28d27ea2ce0a86be64e988
Asif Mahmud <asiffyy@gmail.com>
2020-08-09 12:18:02 +0600

    created BurgerBuilder component and Layout css module

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import style from './Layout.module.css';
 
 const Layout = (props) => {
     return(
@@ -6,7 +7,7 @@ const Layout = (props) => {
             <div>
                 Toolbar, SideDrawer, BackDrop
             </div>
-            <main>
+            <main className={style.content}>
                 {props.children}
             </main>
         </React.Fragment>
