36728d17b349a864000c76fdfd475820d79f7fe4
Asif Mahmud <asiffyy@gmail.com>
2020-08-09 13:29:43 +0600

    created BurgerIngredient

---
+++ src/components/Buger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import style from './BurgerIngredient.module.css';
+
+
+const BurgerIngredient = (props) => {
+    let ingredient = null;
+
+    if(props.type === 'bread-bottom'){
+        ingredient = <div className={style.BreadBottom}></div>
+    }else if(props.type === 'bread-top'){
+        ingredient = (
+            <div className={style.BreadTop}>
+                <div className={style.Seed1}></div>
+                <div className={style.Seed2}></div>
+            </div>
+        );
+    }else if(props.type === 'meat'){
+        ingredient = <div className={style.Meat}></div>
+    }else if(props.type === 'cheese'){
+        ingredient = <div className={style.Cheese}></div>
+    }else if(props.type === 'bacon'){
+        ingredient = <div className={style.Bacon}></div>
+    }else if(props.type === 'salad'){
+        ingredient = <div className={style.Salad}></div>
+    }else {
+        ingredient = null;
+    }
+
+    return ingredient;
+};
+
+export default BurgerIngredient;
