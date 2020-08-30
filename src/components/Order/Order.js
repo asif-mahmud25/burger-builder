422d5c196c37fdb6c3d11dbd392293c47b1e857d
Asif Mahmud <asiffyy@gmail.com>
2020-08-30 12:28:12 +0600

    completed orders page

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,10 +2,25 @@ import React from 'react';
 
 const Order = (props) => {
 
+    const ingredients = [];
+
+    for (let i in props.ingredients){
+        ingredients.push({
+            name: i,
+            ammout: props.ingredients[i]
+        });
+    }
+
+    let outputIngredients = ingredients.map(el => {
+        return(
+            <span key={el.name}>{el.name}: {el.ammout}</span>
+        );
+    })
+
     return(
         <div>
-            <p>Ingredients: Salad 1</p>
-            <p><strong>Price: $5.60</strong></p>
+            <p>{outputIngredients}</p>
+            <p><strong>Price: {props.price.toFixed(2)}</strong></p>
         </div>
     );
 }
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,13 +1,52 @@
 import React from 'react';
 import Order from '../../components/Order/Order';
+import axios from 'axios';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 
 class Orders extends React.Component {
 
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        let fetchedData = [];
+        axios.get('https://burger-builder-7fbdc.firebaseio.com/orders.json')
+             .then(response => {
+                 for(let i in response.data){
+                     fetchedData.push({
+                        ...response.data[i],
+                        id: i
+                     });
+                 }
+
+                 this.setState({loading: false, orders: fetchedData});
+             })
+             .catch(error => {
+                 console.log(error);
+                 this.setState({loading: false});
+             })
+    }
+
     render() {
+
+        let orders = <Spinner />;
+
+        if(this.state.loading === false){
+            orders = this.state.orders.map(el => {
+                return(
+                    <Order ingredients={el.ingredients}
+                        price={+el.price}
+                        key={el.id}/>
+                )
+            });
+        }
+
         return(
             <div>
-                <Order />
+                {orders}
             </div>
         );
     }
