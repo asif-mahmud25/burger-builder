2bacb9638f7a7ccf8cdf1dff784f3f2ff0bb8ff2
Asif Mahmud <asiffyy@gmail.com>
2020-09-24 16:23:49 +0600

    added routing guards

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+import { connect } from 'react-redux';
 
-function App() {
+function App(props) {
+
+  let routes = (
+    <Switch>
+      <Route path="/" exact component={BurgerBuilder} />
+      <Route path="/checkout" component={Checkout} />
+      <Route path="/auth" component={Auth} />
+      <Redirect to="/" />
+    </Switch>
+  );
+
+  if (props.isAuth) {
+    routes = (
+      <Switch>
+        <Route path="/" exact component={BurgerBuilder} />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/orders" component={Orders} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/auth" component={Auth} />
+        <Redirect to="/" />
+      </Switch>
+    )
+  }
 
   return (
     <BrowserRouter>
       <div>
-          <Layout>          
-            <Route path="/" exact component={BurgerBuilder}/>
-            <Route path="/checkout" component={Checkout}/>
-            <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/logout" component={Logout} />
-          </Layout>
+        <Layout>
+          {/* <Route path="/" exact component={BurgerBuilder} />
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/auth" component={Auth} />
+          <Route path="/logout" component={Logout} /> */}
+          {routes}
+        </Layout>
       </div>
     </BrowserRouter>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    isAuth: state.auth.idToken !== null
+  }
+}
+
+export default connect(mapStateToProps)(App);
